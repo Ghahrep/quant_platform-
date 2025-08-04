a088d61b4db9d9f6d6d3238041b3c2afe0dc12a0
Ghahre Pascale <ghahre@gmail.com>
2025-08-04 11:17:36 -0500

    Initial commit: Project scaffolding

--- gertie-frontend/src/components/ui/index.ts
+++
@@ -1,6 +0,0 @@
-export { Card, CardHeader, CardContent, CardFooter } from './Card';
-export { Button } from './Button';
-export { LoadingSpinner } from './LoadingSpinner';
-export { Badge } from './Badge';
-export { Toaster } from './Toaster';
-export { ErrorBoundary } from './ErrorBoundary';
